971439be2b7cd7cfff44112aab19e9a6e4eb4e9c
MiguelCh <migueldrums741@gmail.com>
2021-09-16 16:51:43 -0400

    Empece a crear los redireccionamientos a las diferentes secciones del menu en la pagina principal, usando html

--- docs/Web-application.rkt
+++ docs/Web-application.rkt
@@ -13,26 +13,25 @@
              (header
               (nav ((class "navegacion"))
                      (ul ((class "menu"))
-        
-                           (li (a ((href "")) "Hardware")
+                                
+                           (li (a ((href "hardware-section.html")) "Hardware")
                                (div
                                 (ul ((class "Submenu"))
                                       (li ((class "Titulo"))
                                         (h1 "Computadoras y accesorios Raspberry Pi de bajo costo y alto rendimiento"))
                                       (p (a ((href "")) (button ((class "button-submenu")) "Comprar Raspberry Pi 4")))
                                       (p (a ((href "")) (button ((class "button-submenu")) "Comprar Raspberry Pi 400")))
-
                                       (li (a ((href "")) "Todos los productos"))
                                       (li (a ((href "")) "Para la industria"))
                                       (li (a ((href "")) "Documentación"))
                                       (li (a ((href "")) "Foros")))))
                            
-                           (li (a ((href "")) "Software")
+                           (li (a ((href "software-section.html")) "Software")
                                (div
                                 (ul ((class "Submenu"))
                                      (li ((class "Titulo"))
                                        (h1 "Todo lo que necesita para comenzar con su computadora Raspberry Pi"))
-                                     (link (a ((href "")) (button ((class "button-submenu")) "Nuestro software")))
+                                     (p (a ((href "")) (button ((class "button-submenu")) "Nuestro software")))
                                      (li (a ((href "")) "Raspberry Pi OS"))
                                      (li (a ((href "")) "Raspberry Pi Desktop"))
                                      (li (a ((href "")) "Ayuda"))
@@ -43,7 +42,7 @@
                                 (ul ((class "Submenu"))
                                      (li ((class "Titulo"))
                                        (h1 "Libros y revistas de Raspberry Pi Press"))
-                                     (link (a ((href "")) (button ((class "button-submenu")) "Explore nuestros titulos")))
+                                     (p (a ((href "")) (button ((class "button-submenu")) "Explore nuestros titulos")))
                                      (li (a ((href "")) "El MagPi"))
                                      (li (a ((href "")) "HackSpace"))
                                      (li (a ((href "")) "Estructura alámbrica"))
@@ -54,7 +53,7 @@
                                 (ul ((class "Submenu"))
                                      (li ((class "Titulo"))
                                       (h1 "Recursos gratuitos para que los jóvenes aprendan a codificar y se conviertan en creadores digitales"))
-                                     (link (a ((href "")) (button ((class "button-submenu")) "Aprender en casa")))
+                                     (p (a ((href "")) (button ((class "button-submenu")) "Aprender en casa")))
                                      (li (a ((href "")) "Proyectos de codificación guiada"))
                                      (li (a ((href "")) "Para la industria"))
                                      (li (a ((href "")) "Aprende en un CoderDojo")))))
@@ -64,7 +63,7 @@
                                 (ul ((class "Submenu"))
                                      (li ((class "Titulo"))
                                       (h1 "Capacitación, recursos y orientación gratuitos para ayudarlo a enseñar informática con confianza."))
-                                     (link (a ((href "")) (button ((class "button-submenu")) "Apoyo a los profesores")))
+                                     (p (a ((href "")) (button ((class "button-submenu")) "Apoyo a los profesores")))
                                      (li (a ((href "")) "Enseñar Computación"))
                                      (li (a ((href "")) "Iniciar un club de códigos"))
                                      (li (a ((href "")) "Cursos de formación online")))))
@@ -74,7 +73,7 @@
                                 (ul ((class "Submenu"))
                                      (li ((class "Titulo"))
                                       (h1 "Trabajamos para poner el poder de la informática y la creación digital en manos de personas de todo el mundo."))
-                                     (link (a ((href "")) (button ((class "button-submenu")) "Sobre nosotros")))
+                                     (p (a ((href "")) (button ((class "button-submenu")) "Sobre nosotros")))
                                      (li (a ((href "")) "Donar"))
                                      (li (a ((href "")) "Apoyanos"))
                                      (li (a ((href "")) "Investigar"))
@@ -90,12 +89,14 @@
                      (link (a ((href "")) (button ((class "button-two")) "Empieza a usar tu Raspberry Pi 400")))
                      (link (a ((href "")) (button ((class "button-three"))"Raspberry Pi 400 para trabajar y aprender en casa")))))))))
 
-
 (serve/servlet start
                #:listen-ip #f
+               #:launch-browser? #t
+               #:quit? #f
                #:servlet-path "/ubiotstore.rkt"
                #:port 8000
                #:extra-files-paths
                 (list
                  (build-path "/home/training1/Migue/Pagina-Web/docs/htdocs")
-                 (build-path "/home/training1/Migue/Pagina-Web/docs/images")))
+                 (build-path "/home/training1/Migue/Pagina-Web/docs/images")
+                 (build-path "/home/training1/Migue/Pagina-Web/docs")))
